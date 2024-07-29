87e2aa340e3e4dd52f0c9b6f418e64c0ce7c529d
chltmdgh522 <chltmdgh517@naver.com>
2024-07-30 01:02:31 +0900

    완벽 초기 세팅

---
+++ character/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def character_list(request):
+    return 0
+
+
+def character_list(request):
+    return 0
+
+
+def character_create(request):
+    return 0
+
+
+def character_create_detail(request):
+    return 0
+
+
+def character_update(request):
+    return 0
+
+
+def trash_delete(request):
+    return 0
+
+
+def trash(request):
+    return 0
+
+
+def trash_restore(request):
+    return 0
+
+def trash_perfect_delete(request):
+    return 0
