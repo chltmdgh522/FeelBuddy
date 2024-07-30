659ba3b04584bc8e90be6f8e662ef86ff3d27834
chltmdgh522 <chltmdgh517@naver.com>
2024-07-30 23:04:16 +0900

    1차 챗봇 코드 수정

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,19 +1,54 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from character.models import UserCharacter
+from chatbot.models import ChatbotAIContent, ChatbotUserContent
 
 
 # Create your views here.
 
-def chatbot_content_list(request):
-    return 0
+@login_required
+def chatbot_content_list(request, pk):
+    user = request.user
+    character = UserCharacter.objects.get(id=pk, user=user)
+    ai_contents = ChatbotAIContent.objects.filter(user=user, character=character).order_by('create_ai_time')
+    user_contents = ChatbotUserContent.objects.filter(user=user, character=character).order_by('create_user_time')
+    context = {
+        'ai_contents': ai_contents,
+        'user_contents': user_contents,
+    }
+
+    return render(request, 'chatbot/chatbotContentList.html', context)
 
 
-def chatbot_ai_create(request):
-    return 0
+@login_required
+def chatbot_ai_create(request, pk):
+    if request.method == 'POST':
+        user = request.user
+        character = UserCharacter.objects.get(id=pk, user=user)
+        ChatbotAIContent.objects.create(
+            user=user,
+            userCharacter=character,
+            ai_content=request.POST.get('ai_content'),
+        )
+        return request.POST.get('ai_content')
+    return "get은 없음"
 
 
-def chatbot_user_create(request):
-    return 0
+@login_required
+def chatbot_user_create(request, pk):
+    if request.method == 'POST':
+        user = request.user
+        character = UserCharacter.objects.get(id=pk, user=user)
+        ChatbotUserContent.objects.create(
+            user=user,
+            userCharacter=character,
+            user_content=request.POST.get('user_content'),
+        )
+        return request.POST.get('user_content')
+    return "get은 없음"
 
 
+@login_required
 def emotion(request):
     return 0
