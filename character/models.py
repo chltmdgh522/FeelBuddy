2b32c5e091ed2120193f2745133671bd90da49f7
minsoo lee <minsoolee@minsooui-MacBookPro.local>
2024-07-31 19:53:21 +0900

    [feat] Character models.py AdminCharacter class에 image 속성 추가

--- character/models.py
+++ character/models.py
@@ -7,12 +7,18 @@ from users.models import User
 # 사용자가 생성한 캐릭터 테이블
 class AdminCharacter(models.Model):
     emotion = models.CharField(max_length=10)
+    image = models.ImageField(upload_to='characters/img/')  #모델 변화 ---> 이미지 필드 추가
+
+    def __str__(self):
+        return self.emotion
 
 
 class UserCharacter(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 유저 식별자1
-    adminCharacter = models.ForeignKey(AdminCharacter,
-                                       on_delete=models.CASCADE)  # 초기 캐릭터 식별자 즉 걸려있는데 5개이니깐 adminCharacter로 부터 외래키를 받아야됨
-    introduce = models.TextField()  # 생성 이유 (사용자가 캐릭터 생성할 때 폼에서 작성하는 코멘트)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 유저 식별자
+    adminCharacter = models.ForeignKey(AdminCharacter, on_delete=models.CASCADE)  # 초기 캐릭터 식별자
+    introduce = models.TextField()  # 생성 이유
     trash = models.BooleanField(default=False)  # 캐릭터 휴지통 플래그
     name = models.CharField(max_length=30)  # 캐릭터 이름
+
+    def __str__(self):
+        return self.name
