42cc9d3f533d7d9353721be62ae2a28f86cd129e
JNMYNG <jmj00327@gmail.com>
2024-08-03 02:41:52 +0900

    signup, login form 데이터 직접 받아오도록 수정

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
-
+from django.contrib.auth import get_user_model
 # Create your views here.
 
 def main(request):
@@ -10,29 +10,30 @@ def main(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        if username and password and email:
+            User = get_user_model()
+            user = User.objects.create_user(username=username, password=password, email=email)
             auth_login(request, user)
             return redirect('users:main')
-    else:
-        form = SignUpForm()
-    return render(request, 'user/signup.html', {'form': form}) 
-
+    return render(request, 'user/signup.html')
 
 def login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                auth_login(request, user)
-                return redirect('users:main')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'user/login.html', {'form': form})
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('users:main')
+        else:
+            # 로그인 실패 시
+            return render(request, 'user/login.html', {'error': 'Invalid username or password'})
+    return render(request, 'user/login.html')
 
 def logout(request):
     auth_logout(request)
