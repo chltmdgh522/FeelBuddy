6c1ffce029e620acb15007e975f8922abeea5be3
chltmdgh522 <chltmdgh517@naver.com>
2024-08-04 16:42:34 +0900

    회원가입 연동

--- users/views.py
+++ users/views.py
@@ -2,22 +2,40 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
+from .models import User
+
 
 # Create your views here.
 
 def main(request):
     return render(request, 'user/test.html')
 
+
 def signup(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            auth_login(request, user)
-            return redirect('users:main')
-    else:
-        form = SignUpForm()
-    return render(request, 'user/signup.html', {'form': form}) 
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        name = request.POST.get('name')
+        print("gsdgafsdfasdfasdf")
+        # 사용자 존재 여부 확인
+        if User.objects.filter(username=username).exists():
+            return render(request, 'user/signup.html', {'error': '이미 사용 중인 사용자 이름입니다.'})
+
+        if User.objects.filter(email=email).exists():
+            return render(request, 'user/signup.html', {'error': '이미 사용 중인 이메일 주소입니다.'})
+        try:
+            user = User.objects.create(
+                username=username,
+                email=email,
+                password=password,
+                name=name  # 'name' 필드를 'first_name'으로 사용
+            )
+            return redirect('users:login')
+        except Exception as e:
+            return render(request, 'user/signup.html', {'error': '회원가입에 실패했습니다.'})
+
+    return render(request, 'user/signup.html')
 
 
 def login(request):
@@ -33,6 +51,7 @@ def login(request):
     else:
         form = AuthenticationForm()
     return render(request, 'user/login.html', {'form': form})
+
 
 def logout(request):
     auth_logout(request)
