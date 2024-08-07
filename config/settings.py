978581209721e2abbca4c11b0ff895544f8b78a2
chltmdgh522 <chltmdgh517@naver.com>
2024-08-07 23:50:47 +0900

    static 파일 루트 권한

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigA utoField'
 
 #구글 소셜로그인 관련 설정
 SITE_ID = 1
