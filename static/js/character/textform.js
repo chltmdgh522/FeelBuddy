f54301e0280a5ddcab29a200722d455e08531006
chltmdgh522 <chltmdgh517@naver.com>
2024-08-08 15:56:02 +0900

    캐릭터 디테일 생성 잇기

---
+++ static/js/character/textform.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const flipCard = document.getElementById('flipCard');
+  const submitCard = document.getElementById('submitCard');
+  const envelope = document.querySelector('.envelope');
+
+  function toggleAnimation() {
+      if (envelope.classList.contains('active')) {
+          flipCard.textContent = 'Animate';
+      } else {
+          flipCard.textContent = '';
+      }
+      envelope.classList.toggle('active');
+  }
+
+  flipCard.addEventListener('click', toggleAnimation);
+
+  submitCard.addEventListener('click', toggleAnimation);
+});
