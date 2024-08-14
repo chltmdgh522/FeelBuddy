1d7912a56e86e4c0b0352c4e6e8a4462a8bb0008
minsoo lee <minsoolee@192.168.nate.com>
2024-08-14 23:23:00 +0900

    [pull] latest pull

--- character/initial_characters.py
+++ character/initial_characters.py
@@ -18,6 +18,7 @@ from django.conf import settings
 def create_initial_characters():
     emotions = ['angry', 'anxiety', 'fear', 'joy', 'sad']
     for emotion in emotions:
-        image_path = f"{settings.MEDIA_URL}characters/{emotion}.png"
+        image_path = f"{emotion}.png"
         AdminCharacter.objects.get_or_create(emotion=emotion, defaults={'image': image_path})
 
+create_initial_characters()
