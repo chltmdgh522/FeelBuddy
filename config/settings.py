0b5431f90b3d51f587eadaea5f707206d684d3ee
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2024-08-18 14:48:03 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-w&ru*a=8k++v5gtnuacib$5(kfes@u!mpzp4@*jclgnv7bu$e&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['121.173.113.60', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -40,7 +42,6 @@ INSTALLED_APPS = [
     'feedback',
     'users',
     'character',
-
 
     # 구글 소셜로그인
     'django.contrib.sites',
@@ -92,9 +93,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),  # MySQL 데이터베이스 이름
+        'USER': env('DB_USER'),  # MySQL 사용자 이름
+        'PASSWORD': env('DB_PASSWORD'),  # MySQL 사용자 비밀번호
+        'HOST': env('DB_HOST'),  # MySQL 서버 주소 (로컬 서버의 경우 'localhost' 또는 '127.0.0.1')
+        'PORT': '3307',  # MySQL 서버 포트 (기본값은 3306)
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -133,7 +142,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # 정적 파일이 실제 서빙될 URL 경로를 설정합니다.
 STATIC_URL = '/static/'
 
@@ -160,32 +169,32 @@ LOGIN_REDIRECT_URL = '/character/list'
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'APP': {
-            'client_id': '375689531334-oa582g2gk7dvoo6nn94ggehd5opvcoeb.apps.googleusercontent.com',
-            'secret': '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',
+            'client_id': env('GOOGLE_CLIENT_ID'),
+            'secret': env('GOOGLE_SECRET'),
             'key': ''
         }
     },
     'naver': {
         'APP': {
-            'client_id': '20M3Zwb2VpH574y0OHQN',
-            'secret': 'iDD27qtz9e',
+            'client_id': env('NAVER_CLIENT_ID'),
+            'secret': env('NAVER_SECRET'),
             'key': ''
         }
     },
     'kakao': {
         'APP': {
-            'client_id': '4703a49559d7241fea4341b24a9b8dd8',
-            'secret': 'FvTb3LFhgpT4jufmI2o3dSwYf33CZE3E',
+            'client_id': env('KAKAO_CLIENT_ID'),
+            'secret': env('KAKAO_SECRET'),
             'key': ''
         }
     }
 }
 SOCIALACCOUNT_AUTO_SIGNUP = True
-#비밀번호 재설정 위한 이메일 설정
+# 비밀번호 재설정 위한 이메일 설정
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jmj00327@gmail.com'
-EMAIL_HOST_PASSWORD = 'qzhr lliw epgp chfw'
+EMAIL_HOST_USER = env('MAIL_ID')
+EMAIL_HOST_PASSWORD = env('MAIL_PW')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
